606f4675e0b367822443672a87cb1134152372ea
Gabriel Viana <gabrielalmeidavn@gmail.com>
2022-07-04 16:57:16 -0300

    resampled the polynomial points to get a better classification

--- src/shape/main.py
+++ src/shape/main.py
@@ -21,7 +21,8 @@ class BcsPolynomialFit:
 
     def create_characteristic_polynomials(self):
         for bcs, image_path in self.__characteristic_bcs_images.items():
-            cow_image, thresh, top_back_shape, x, y, polynomial_coefficients, polynomial = self.__create_polynomial(image_path)
+            cow_image, thresh, top_back_shape, x, y, polynomial_coefficients, polynomial = self.__create_polynomial(
+                image_path)
             self.__characteristic_bcs_info[bcs] = {
                 "image": cow_image,
                 "thresh": thresh,
@@ -32,26 +33,42 @@ class BcsPolynomialFit:
                 "polynomial": np.poly1d(polynomial_coefficients)
             }
 
-    # QUESTION: When I use the polynomial coefficients to measure the MSE the it works, but doesn't make sense to me,
-    # because we need to pass the y values to MSE function, so I think the correct is to create a range for the x values
-    # and pass it to the polynomials, but when I do this, the results are worse than before.
-    def predict(self, image_path: str) -> float:
+    def predict(self, image_path: str, real_bcs: float) -> float:
         mse_scores = {}
-        cow_image, thresh, top_back_shape, x, y, polynomial_coefficients, polynomial = self.__create_polynomial(image_path)
+        cow_image, thresh, top_back_shape, x, y, polynomial_coefficients, polynomial = self.__create_polynomial(
+            image_path)
 
-        # x = np.arange(-40, 40)
+        i = 0
+        j = 0
+        fig, ax = plt.subplots(2, 4, figsize=(15, 10))
 
         for bcs, info in self.__characteristic_bcs_info.items():
-            # mse_scores[bcs] = mean_squared_error(info["polynomial"](x), polynomial(x))
-            mse_scores[bcs] = mean_squared_error(info["polynomial"], polynomial)
+            new_x_real = np.linspace(sorted(info["x"])[0], sorted(info["x"])[-1], 500)
+            new_y_real = info["polynomial"](new_x_real)
+
+            new_x_predict = np.linspace(sorted(x)[0], sorted(x)[-1], 500)
+            new_y_predict = polynomial(new_x_predict)
+
+            mse_scores[bcs] = mean_squared_error(new_y_real, new_y_predict)
+
+            if j > 3:
+                i += 1
+                j = 0
+
+            ax[i, j].plot(new_x_real, new_y_real, "o", markersize=2, color="orange")
+            ax[i, j].plot(new_x_predict, new_y_predict, "o", markersize=2)
+            ax[i, j].legend([f"known poly - ECC: {bcs}", f"poly to be predicted - ECC: {real_bcs}"], loc="best")
+
+            j += 1
+
+        print(mse_scores)
+        plt.show()
 
         return float(min(mse_scores, key=mse_scores.get))
 
     def show_characteristic_polynomials(self):
         for bcs, info in self.__characteristic_bcs_info.items():
-            fig, ax = plt.subplots(1, 4, figsize=(14, 6))
-            fig.set_figwidth(14)
-            fig.set_figheight(6)
+            fig, ax = plt.subplots(1, 5, figsize=(15, 5))
 
             ax[0].imshow(cv2.cvtColor(info["image"], cv2.COLOR_GRAY2RGB))
             ax[0].set_title(f"Cow BCS = {bcs}")
@@ -62,13 +79,22 @@ class BcsPolynomialFit:
             ax[2].imshow(cv2.cvtColor(info["top_back_shape"], cv2.COLOR_GRAY2RGB))
             ax[2].set_title("Contour")
 
-            x_range = (info["top_back_shape"].shape[1] - 1) // 2
-            y_range = (info["top_back_shape"].shape[0] - 1) // 2
-            plt.xlim([-x_range, x_range])
-            plt.ylim([-y_range, y_range])
+            polynomial_results = info["polynomial"](info["x"])
+
             ax[3].plot(info["x"], info["y"], "o", markersize=2, color="orange")
-            ax[3].plot(info["x"], np.polyval(info["polynomial_coefficients"], info["x"]), "o", markersize=3)
+            ax[3].plot(info["x"], polynomial_results, "o", markersize=3)
             ax[3].set_title(f"Polynomial degree = {self.__polynomial_degree}")
+            ax[3].legend(["cow points", "polynomial"], loc="best")
+
+            new_x_test = np.linspace(sorted(info["x"])[0], sorted(info["x"])[-1], 500)
+            new_y_test = info["polynomial"](new_x_test)
+
+            ax[4].plot(new_x_test, new_y_test, "o", markersize=3, color="orange")
+            ax[4].plot(info["x"], polynomial_results, "o", markersize=3)
+            ax[4].set_title("Resample")
+            ax[4].legend(["resampled", "polynomial"], loc="best")
+
+            # break
         plt.show()
 
     def __create_polynomial(self, image_path: str):
@@ -78,8 +104,12 @@ class BcsPolynomialFit:
         blur_image = cv2.blur(thresh, self.__kernel_size)
         blur_image[blur_image != 255] = 0
 
-        erode_image = self.__erode(blur_image, kernel_size=self.__kernel_size)
-        subtract_image = cv2.subtract(blur_image, erode_image)
+        # dilate the image to prevent black pixels in the center of the cow
+        kernel = np.ones(self.__kernel_size, np.uint8)
+        dilated_image = cv2.dilate(blur_image, kernel, iterations=2)
+
+        erode_image = self.__erode(dilated_image, kernel_size=self.__kernel_size)
+        subtract_image = cv2.subtract(dilated_image, erode_image)
         top_back_shape = self.__get_top_back_shape(subtract_image)
 
         x, y = self.__translate_shape_coords_to_origin(top_back_shape)
@@ -120,7 +150,7 @@ class BcsPolynomialFit:
         return trans_x, trans_y
 
 
-if __name__ == "__main__":
+def main():
     images_path = os.path.abspath('../../images/grabcut')
 
     bcs_polynomial_fit = BcsPolynomialFit()
@@ -139,6 +169,8 @@ if __name__ == "__main__":
 
     results = {"right": 0, "wrong": 0}
 
+    # print(f'The predicted bcs is: {bcs_polynomial_fit.predict(images_path + os.sep + "ECC_3.0" + os.sep + "grabcut_4.png", 3.0)}')
+
     # directory[0] -> directory path
     # directory[1] -> subdirectories names
     # directory[2] -> directory files
@@ -146,13 +178,18 @@ if __name__ == "__main__":
         if len(directory[1]) == 0:
             for image_file in directory[2]:  # walk through the image files in directories
                 test_cow = directory[0] + os.sep + image_file
-                real_bcs = float(directory[0].split(os.sep)[-1].split("_")[-1])  # get the BCS number from the directory name
+                real_bcs = float(
+                    directory[0].split(os.sep)[-1].split("_")[-1])  # get the BCS number from the directory name
 
                 if train_images[real_bcs] != test_cow:  # check if the test image is the train image
-                    predicted_bcs = bcs_polynomial_fit.predict(test_cow)
+                    predicted_bcs = bcs_polynomial_fit.predict(test_cow, real_bcs)
                     if real_bcs == predicted_bcs:
                         results["right"] += 1
                     else:
                         results["wrong"] += 1
 
     print(results)
+
+
+if __name__ == "__main__":
+    main()
